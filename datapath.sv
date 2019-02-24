07b200265712f6172233349f0ab01bd11d7b7e9a
stanleyzhaoh2o <47932487+stanleyzhaoh2o@users.noreply.github.com>
2019-02-23 21:23:45 -0600

    datapath modifies sets the NZP values

--- datapath.sv
+++ datapath.sv
@@ -1,6 +1,6 @@
 module datapath
 (
-		input logic Clk, Reset, LD_MDR, LD_MAR, LD_IR, LD_PC,
+		input logic Clk, Reset, LD_MDR, LD_MAR, LD_IR, LD_PC, LD_CC,
 		input logic GateMDR, GatePC, GateALU, GateMARMUX, MIO_EN,
 		input logic [1:0] PCMUX,
 		input logic [15:0] Data_from_SRAM, 
@@ -9,6 +9,8 @@ module datapath
 );
 
 		logic [15:0] MDRval, BUS, PCplus1, PCval;
+		logic N_in, Z_in, P_in;
+		logic N_out, Z_out, P_out;
 		
 		//load to BUS
 		BUS_MUX BUSnew (.d0(PC), .d1(MDR), .d2(/*ALU*/ 16'b0), .d3(/*MARMUX*/ 16'b0), .s({GatePC, GateMDR, GateALU, GateMARMUX}), .y(BUS));
@@ -25,5 +27,32 @@ module datapath
 		register IR1(.Clk(Clk), .Reset(Reset), .Load(LD_IR), .Data_in(BUS), .Data_out(IR));
 		mux4 pcmux(.d0(PCplus1), .d1(16'b0), .d2(16'b0), .d3(16'b0), .s(PCMUX), .y(PCval));
 		register PC1(.Clk(Clk), .Reset(Reset), .Load(LD_PC), .Data_in(PCval), .Data_out(PC));
-
+		
+		NZP nzp(.*);
+		
+		always_comb begin
+			if (BUS[15]) begin
+				N_in = 1'b1;
+				Z_in = 1'b0;
+				P_in = 1'b0;
+			end
+			else begin
+				case (BUS)
+					16'b0:
+						begin
+							N_in = 1'b0;
+							Z_in = 1'b1;
+							P_in = 1'b0;
+						end
+				default:
+				begin
+					N_in = 1'b0;
+					Z_in = 1'b0;
+					P_in = 1'b0;
+				end
+				endcase
+			end
+		end
+			
+							
 endmodule
