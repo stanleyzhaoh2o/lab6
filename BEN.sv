06a15b7a1d97252b1908c57fbeb7c0947877e4bc
stanleyzhaoh2o <47932487+stanleyzhaoh2o@users.noreply.github.com>
2019-02-23 20:57:18 -0600

    Create BEN.sv
    created BEN.sv, this will produce the BEN signal which will act as an input for ISDU

---
+++ BEN.sv
@@ -0,0 +1,18 @@
+module BEN
+(
+		input logic Clk,
+		input logic N,
+		input logic Z,
+		input logic P,
+		input logic LD_BEN,
+		input logic [2:0] IR_sub,
+		output logic BEN
+);
+
+always_ff @ (posedge Clk)
+		begin 
+			if (LD_BEN)
+				BEN <= (IR_sub[2]&N)|(IR_sub[1]&Z)|(IR_sub[0]&P);
+		end
+endmodule
+		
