26082f2839502eda1e4975dfa4371044e6c491ce
xwshen <46983936+xwshen@users.noreply.github.com>
2019-02-24 00:26:39 -0600

    Create ALU.sv

---
+++ ALU.sv
@@ -0,0 +1,21 @@
+module ALU 
+(
+input  logic [15:0] A, B,
+input  logic [1:0]  ALUK,
+output logic [15:0] OUT
+);
+				
+always_comb
+	begin
+		case (Select)		
+			2'b00:
+				OUT = A + B;// add
+			2'b01:
+				OUT = A & B;// and
+			2'b10:
+				OUT = ~A;	// not A
+			2'b11:
+				OUT = A;		// A
+		endcase
+	end	
+endmodule
